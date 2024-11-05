c41c465b8481704d7296a3aff0688e8d261fc5ff
23CadenS <143567564+23CadenS@users.noreply.github.com>
2024-11-05 10:02:08 -0600

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
     const targetUrl = event.queryStringParameters.url;
-    
+
     if (!targetUrl) {
         return {
             statusCode: 400,
@@ -12,11 +12,20 @@ exports.handler = async function(event, context) {
 
     try {
         const response = await fetch(targetUrl);
-        const data = await response.text();
-        
+        let data = await response.text();
+
+        // Rewrite all URLs to go through the proxy
+        const baseProxyUrl = `${event.rawUrl.split('?')[0]}?url=`;
+        data = data.replace(/(href|src)=["'](\/[^"']*)["']/g, (match, attr, path) => {
+            return `${attr}="${baseProxyUrl}${encodeURIComponent(new URL(path, targetUrl).href)}"`;
+        });
+
         return {
             statusCode: 200,
-            body: data
+            body: data,
+            headers: {
+                "Content-Type": response.headers.get("content-type")
+            }
         };
     } catch (error) {
         return {
